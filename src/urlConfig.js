098ddefd04ad045f67507393b88b7598372a3b1c
tran minh hieu <tranminhhieu9901@gmail.com>
2021-03-19 10:27:40 +0700

    fixed img url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -3,5 +3,5 @@ const baseUrl = "https://flipkart-backend-1.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePicture = (picName) => {
-    return `${baseUrl}/${picName}`
+    return `${baseUrl}/public/${picName}`
 }
